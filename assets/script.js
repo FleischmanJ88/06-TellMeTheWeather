79a29e109e6ee3b6d8cf689ad62e9bd43864d7fe
Jeff Fleischman <FleischmanJ88@gmail.com>
2022-10-09 00:27:01 -0500

    javascript updates for API key useage

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,37 @@
-var APIKey = "0d293cb76a4297200575f4c5a735a2a2"
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&0d293cb76a4297200575f4c5a735a2a2=" + APIKey;
+const form = document.querySelector('#form')
+const input = document.querySelector('#input')
+const feedbackElement = document.querySelector('#feedback')
+var apiKey = '0d293cb76a4297200575f4c5a735a2a2'
+
+
+  // fetch the weather 
+  function getWeather(location) {
+    var { lat } = location
+    var { lon } = location
+
+    console.log(location)
+
+    const url = `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    console.log('this is from get weather', url)
+  }
+
+
+// refactor to a function that fetches coordinates 
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    let inputVal = input.value
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&limit=5&appid=${apiKey}`
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(function (data) {
+      getWeather(data)
+    })
+   
+      //feedbackElement.textContent = data
+    .catch(() => {
+   feedbackElement.textContent = "Please search for valid location"
+   })
+  })
