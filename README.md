b3914deb10f87a79b109e6bdc074498b9197110f
Jeff Fleischman <FleischmanJ88@gmail.com>
2022-10-10 21:50:00 -0500

    adding Readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # 06-TellMeTheWeather
+
+## <strong>Description</strong>
+
+For Module 6, we are tasked with using a weather API to create a weather tracking website
+
+## <strong>Acceptance Criteria</strong>
+
+♦ When searching for a city, you are presented with future weather conditions for that city and added to history
+
+♦ When viewing the weather, you are presented with city name, date, condititions, temp, humidity
+
+♦ When viewing, there is an icon representation of weather conditions, temp, speed, and humidity
+
+## <strong>Criteria Met</strong>
+
+♦ Basic criteria framework met for HTML and CSS of website
+
+♦ 1/2 of the API Javascript fulfilled
+
+♦ Unable to have enough time to complete this project
+
+### <strong>Website Link</strong>
+
+https://github.com/FleischmanJ88/06-TellMeTheWeather
+
+### <strong>Screenshot</strong>
+
+![](assets/WeatherDashboard.png)
+
+### <strong>Contact Information</strong>
+
+https://github.com/FleischmanJ88
--- assets/script.js
+++ assets/script.js
@@ -4,34 +4,35 @@ const feedbackElement = document.querySelector('#feedback')
 var apiKey = '0d293cb76a4297200575f4c5a735a2a2'
 
 
-  // fetch the weather 
-  function getWeather(location) {
-    var { lat } = location
-    var { lon } = location
+// fetch the weather 
+function getWeather(location) {
+  var { lat } = location
+  var { lon } = location
 
-    console.log(location)
+  console.log(location)
 
-    const url = `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${lat}&lon=${lon}&appid=${apiKey}`
-    console.log('this is from get weather', url)
-  }
+  const url = `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${lat}&lon=${lon}&appid=${apiKey}`
+  console.log('this is from get weather', url)
+}
 
 
 // refactor to a function that fetches coordinates 
 form.addEventListener("submit", e => {
-    e.preventDefault();
+  e.preventDefault();
 
-    let inputVal = input.value
+  let inputVal = input.value
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&limit=5&appid=${apiKey}`
-    
-    fetch(url)
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&limit=5&appid=${apiKey}`
+
+  fetch(url)
     .then(response => response.json())
     .then(function (data) {
       getWeather(data)
     })
-   
-      //feedbackElement.textContent = data
+
+    //feedbackElement.textContent = data
     .catch(() => {
-   feedbackElement.textContent = "Please search for valid location"
-   })
-  })
+      feedbackElement.textContent = "Please search for valid location"
+    })
+})
+
